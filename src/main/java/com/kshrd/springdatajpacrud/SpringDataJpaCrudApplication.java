8a3c18f3998f365a330d75f08b1edf662a7b1135
chantha <chanthameng1432@gmail.com>
2024-12-26 11:15:39 +0700

    init project

---
+++ src/main/java/com/kshrd/springdatajpacrud/SpringDataJpaCrudApplication.java
@@ -0,0 +1,13 @@
+package com.kshrd.springdatajpacrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaCrudApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/kshrd/springdatajpacrud/entity/User.java
@@ -0,0 +1,4 @@
+package com.kshrd.springdatajpacrud.entity;
+
+public class User {
+}
